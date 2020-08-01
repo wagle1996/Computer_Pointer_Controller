f8c3e4e9606805b8f94c64869da19b37e9f8d322
Nimisha Wagle <wagle1996@gmail.com>
2020-08-01 13:19:14 +0545

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,8 +5,7 @@ import cv2
 import numpy as np
 import math
 from argparse import ArgumentParser
-import logging as logging
-
+import logging as log
 from face_detection import FaceDetect
 from facial_landmarks_detection import FacialLandmarks
 from gaze_estimation import GazeEstimation
@@ -54,15 +53,21 @@ def build_argparser():
                              "hp for headPoseEstimationModel, gze for gazeEstimationModel")
 
     return parser
+def view_eye_rectangle(eyes_coords, eye_buffer,frame):     #idea adapted from https://github.com/yas-sim/gaze-estimation-with-laser-sparking/blob/master/gaze-estimation.py
+    le_xmin,le_ymin = (eyes_coords[0][0]-eye_buffer, eyes_coords[0m][1]-eye_buffer)
+    le_xmax,le_ymax = (eyes_coords[0][2]+eye_buffer, eyes_coords[0][3]+eye_buffer) 
+
+    re_xmin,re_ymin = (eyes_coords[1][0]-eye_buffer, eyes_coords[1][1]-eye_buffer)
+    re_xmax,re_ymax = (eyes_coords[1][2]+eye_buffer, eyes_coords[1][3]+eye_buffer)
+    cv2.rectangle(frame, (le_xmin, le_ymin), (le_xmax, le_ymax), (0,255,0), 3) 
+    cv2.rectangle(frame, (re_xmin, re_ymin), (re_xmax, re_ymax), (0,255,0), 3) 
 
 
 def main():
     args = build_argparser().parse_args()
-    logger = logging.getLogger('main')
+    
+
     # initialize variables with the input arguments for easy access
-
-
-
     fdm=args.face_detection_model
     ldm=args.facial_landmarks_detection_model
     hpem=args.head_pose_estimation_model
@@ -78,7 +83,7 @@ def main():
         feeder = InputFeeder(input_type='cam')
     else:
         if not os.path.isfile(input_filename):
-            logger.error("Unable to find specified video file")
+            log.error("Unable to find specified video file")
             exit(1)
         feeder = InputFeeder(input_type='video', input_file=input_filename)
 
@@ -92,26 +97,32 @@ def main():
 
     # load Models
     start_model_load_time = time.time()
-    face_detection_model.load_model()
-    logger.info("Face Detection Model Loaded...")
+    face_detection_model.load_model() #load face detection model
+    log.info("Face Detection Model Loaded...")
     FDMT=time.time()-start_model_load_time
     start1=time.time()
-    landmark_detection_model.load_model()
-    logger.info("landmark_estimation Model Loaded...")
+    landmark_detection_model.load_model()  #load_landmark_detection_model
+    log.info("landmark_estimation Model Loaded...")
     LDMT=time.time()-start1
     start2=time.time()
-    head_pose_estimation_model.load_model()
-    logger.info("Head pose estimation model Loaded...")
+    head_pose_estimation_model.load_model()  #load_head_pose_estimation_model
+    log.info("Head pose estimation model Loaded...")
     hpem=time.time()-start2
     start3=time.time()
-    gaze_estimation_model.load_model()
-    logger.info("Gaze_estimation model loaded..")
+    gaze_estimation_model.load_model()  #load_gaze_estimation_model
+    log.info("Gaze_estimation model loaded..")
     gem=time.time()-start3
-    total_model_load_time = time.time() - start_model_load_time
+    total_time = time.time() - start_model_load_time
 
     feeder.load_data()
     
-     
+    #check for output flags
+    if (len(output_flags)!=0):
+        for flag in output_flags:
+           if not flag in ['fdm', 'lrm', 'hp', 'gze']:
+                log.error("Flag '" + flag + "' is not a valid preview flag.")
+                sys.exit(1)
+  
     frame_count = 0
     start_inference_time = time.time()
     for ret, frame in feeder.next_batch():
@@ -120,17 +131,19 @@ def main():
             break
         
         frame_count += 1
-        if frame_count%5==0:
-            cv2.imshow('video',cv2.resize(frame,(500,500)))
+        #if frame_count%5==0:
+            #cv2.imshow('video',cv2.resize(frame,(500,500)))
         key = cv2.waitKey(60)
         try:
             
             image,fc = face_detection_model.predict(frame,args.prob_threshold)
+            #print (fc)
+            #print (image.shape)
             #face_cords1=face_cords[0]
             #face_c = face_cords1.astype(np.int8)
             #print (image.shape)
             if type(image) == int:
-                logger.warning("Unable to detect the face")
+                log.warning("Unable to detect the face")
                 if key == 27:
                     break
                 continue
@@ -141,61 +154,49 @@ def main():
                     # crop face from frame
             #face = image[face_cords1[1]:face_cords1[3],face_cords1[0]:face_cords1[2]]
             #print (face.shape)
-            left_eye_image, right_eye_image, eye_coords = landmark_detection_model.predict(image) #using the output of face detection model 
+            if 'fdm' in output_flags:
+                #cv2.rectangle(frame,(fc[0],fc[1]),(fc[2],fc[4]),3)
+                cv2.putText(frame,"face detected",(10,140),cv2.FONT_HERSHEY_COMPLEX,2,(0,0,255),4)
+            # predicting using landmark detection model
+            left_eye_image, right_eye_image, eye_coords = landmark_detection_model.predict(image) #using the output of face detection model
+            print (eye_coords)
+            eye_buffer=10 
+            
+            if 'lrm' in output_flags:
+                view_eye_rectangle(eye_coords,eye_buffer,image)    
+            
             print (left_eye_image.shape)
             print (right_eye_image.shape)
+
+            #predicting using head_pose_estimation model
             pose_output = head_pose_estimation_model.predict(image)
-            mouse_coord, gaze_vector = gaze_estimation_model.predict(left_eye_image, right_eye_image, pose_output)
             yaw = pose_output[0]
             pitch = pose_output[1]
             roll = pose_output[2]
-            focal_length = 950.0
-            scale = 50
-            eye_buffer=10
-            center_of_face = (image.shape[1] / 2, image.shape[0] / 2, 0)
-            logger.info("processing frame "+str(frame_count))
+
+            if "hp" in output_flags:
+                cv2.putText(frame, "Pose Angles: yaw:{:.2f},  pitch:{:.2f},  roll:{:.2f}".format(yaw, pitch, roll), (10, 40), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 0), 4)
+ 
+            mouse_coord, gaze_vector = gaze_estimation_model.predict(left_eye_image, right_eye_image, pose_output)
+            if "gze" in output_flags:
+                cv2.putText(frame, "Gaze Cords: x= {:.2f} , y= {:.2f} , z= {:.2f}".format(gaze_vector[0], gaze_vector[1], gaze_vector[2]), (10, 90),cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 4)
+       
         except Exception as e:
-            logger.warning("Could not predict using model " + str(e) + " for frame " + str(frame_count))
+            log.warning("Could not predict using model " + str(e) + " for frame " + str(frame_count))
             continue
 
         #image = cv2.resize(frame, (500, 500))
         total_inference_time=time.time()-start_inference_time
-        
-        
-        if (len(output_flags)!=0):
-            
-            preview_frame = frame
-            
-            #for Face detection model
-            if 'fdm' in output_flags:
-                preview_frame = image
-                cv2.rectangle(image,(fc[1],fc[3]),(fc[0],fc[2]),(0,0,255),3)
-            
-            #for landmark detection Model    
-            if 'lrm' in output_flags:
-                cv2.rectangle(image, (eye_coords[0][0]-eye_buffer, eye_coords[0][1]-eye_buffer), (eye_coords[0][2]+eye_buffer, eye_coords[0][3]+eye_buffer), (0,255,0), 3) #10 is eye buffer area from centre
-                cv2.rectangle(image, (eye_coords[1][0]-eye_buffer, eye_coords[1][1]-eye_buffer), (eye_coords[1][2]+eye_buffer, eye_coords[1][3]+eye_buffer), (0,255,0), 3) #10 is the eye buffer area from centre  
-             
-            #for head pose Estimation Model   
-            if 'hp' in output_flags:
-                cv2.putText(preview_frame, "Pose Angles: yaw:{:.2f}  pitch:{:.2f}  roll:{:.2f}".format(yaw, pitch, roll), (10, 20), cv2.FONT_HERSHEY_COMPLEX, 0.25, (255, 0, 0), 1)
 
-            #for gaze estimation model
-            if 'gze' in output_flags:
-                cv2.putText(preview_frame, "Gaze Cords: x= {:.2f} , y= {:.2f} , z= {:.2f}".format(gaze_vector[0], gaze_vector[1], gaze_vector[2]), (10, 30),cv2.FONT_HERSHEY_COMPLEX, 0.25, (0, 255, 0), 1)
-                x, y, w = int(gaze_vector[0]*12), int(gaze_vector[1]*12), 160
-                le = cv2.line(left_eye_image, (x-w, y-w), (x+w, y+w), (255,0,255), 1)    #https://arxiv.org/pdf/1805.04771.pdf
-                re = cv2.line(right_eye_image, (x-w, y-w), (x+w, y+w), (255,0,255), 1)
-
-
-        cv2.imshow("Visualization", cv2.resize(preview_frame,(500,500)))
+        cv2.imshow("Visualization", cv2.resize(frame,(500,500)))
         #out_video.write(preview_frame)
+        #moving_mouse_controller
         if frame_count%5==0:
             mouse_controller.move(mouse_coord[0], -1*mouse_coord[1])    
         if key==27:
                 break
-    logger.error("VideoStream ended...")
-    print("total_model_load time is {:} ms".format(1000* total_model_load_time/frame_count))
+    log.error("VideoStream ended...")
+    print("total_model_load time is {:} ms".format(1000* total_time/frame_count))
     print("fps is {:}".format(int(feeder.get_fps())))
     print("total inference time is{:} ms".format(1000*total_inference_time/frame_count))
     print("fdmt loading time is{:} ms".format(1000*FDMT/frame_count))
@@ -206,4 +207,3 @@ def main():
     feeder.close()
 if __name__ == '__main__':
     main() 
-        
